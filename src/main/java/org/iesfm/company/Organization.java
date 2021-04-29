e56547975caa18aca0842a46f348b4d511e30bb5
albarran99 <raulvezon15@gmail.com>
2021-04-29 13:27:58 +0200

    update project

---
+++ src/main/java/org/iesfm/company/Organization.java
@@ -0,0 +1,13 @@
+package org.iesfm.company;
+
+import java.lang.reflect.Member;
+import java.util.LinkedList;
+import java.util.List;
+
+public abstract class Organization {
+    private int cif;
+    private String name;
+    private String direction;
+    private LinkedList<Member> members;
+
+}
